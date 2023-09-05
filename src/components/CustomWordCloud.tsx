cd291542c3d082a55919aa1f8ad4f3db846c51eb
chetan gupta <chetangupta123raj@gmail.com>
2023-09-05 23:40:12 +0530

    dashboard

---
+++ src/components/CustomWordCloud.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from 'react'
+import D3WordCloud from 'react-d3-cloud';
+import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
+
+type Props = {
+    formattedTopics: { 
+        text: string; 
+        value: number 
+    }[];
+}
+
+const fontSizeMapper = (word: { value: number }) => Math.log2(word.value) * 5 + 16;
+
+const CustomWordCloud = ({ formattedTopics }: Props) => {
+  const router = useRouter();
+  const theme = useTheme();
+  return (
+    <>
+        <D3WordCloud
+        data={formattedTopics}
+        height={550}
+        font="Times"
+        fontSize={fontSizeMapper}
+        rotate={0}
+        padding={10}
+        fill={theme.theme === "dark" ? "white" : "black"}
+        onWordClick={(e, d) => {
+          router.push("/quiz?topic=" + d.text);
+        }}
+        />
+    </>
+  )
+}
+
+export default CustomWordCloud
