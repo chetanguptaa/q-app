54d192fc887d1eb120b9c5b90c16af8bcea40280
chetanguptaa <chetangupta123raj@gmail.com>
2023-09-06 22:30:37 +0530

    updated userAccountNav and UserAvatar

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -11,18 +11,23 @@ import type { User } from 'next-auth'
 import { signOut } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
-import { LockClosedIcon } from '@radix-ui/react-icons';
+import { LogOut } from "lucide-react";
 import UserAvatar from './UserAvatar';
 
 type Props = {
-    user: Pick<User, 'name' | 'email' | 'image'>
+    user: Pick<User, 'name' | 'image' | 'email'>
 }
 
 const UserAccountNav = ({ user }: Props) => {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger>
-          <UserAvatar user={user} />
+          <UserAvatar 
+          className="w-10 h-10"
+          user={{
+            name: user.name || null,
+            image: user.image || null,
+          }} />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-white" align="end">
           <div className="flex items-center justify-start gap-2 p-2">
@@ -49,7 +54,7 @@ const UserAccountNav = ({ user }: Props) => {
             className="text-red-600 cursor-pointer"
           >
             Sign out
-            <LockClosedIcon className="w-4 h-4 ml-2 " />
+            <LogOut className="w-4 h-4 ml-2 " />
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
@@ -58,61 +63,3 @@ const UserAccountNav = ({ user }: Props) => {
   
 
 export default UserAccountNav
-
-
-
-/**
- * "use client";
-
-import UserAvatar from "./UserAvatar";
-import { LogOut } from "lucide-react";
-
-
-
-const UserAccountNav = ({ user }: Props) => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <UserAvatar
-          className="w-10 h-10"
-          user={{
-            name: user.name || null,
-            image: user.image || null,
-          }}
-        />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-white" align="end">
-        <div className="flex items-center justify-start gap-2 p-2">
-          <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
-            {user.email && (
-              <p className="w-[200px] truncate text-sm text-zinc-700">
-                {user.email}
-              </p>
-            )}
-          </div>
-        </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Link href="/">Meow</Link>
-        </DropdownMenuItem>
-
-        <DropdownMenuSeparator />
-
-        <DropdownMenuItem
-          onSelect={(event) => {
-            event.preventDefault();
-            signOut().catch(console.error);
-          }}
-          className="text-red-600 cursor-pointer"
-        >
-          Sign out
-          <LogOut className="w-4 h-4 ml-2 " />
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
-
-export default UserAccountNav;
- */
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { User } from 'next-auth'
-import { Avatar, AvatarFallback } from './ui/avatar'
+import { 
+    Avatar, 
+    AvatarFallback 
+} from './ui/avatar'
 import Image from 'next/image'
+import { type AvatarProps } from "@radix-ui/react-avatar";
 
-type Props = {
-    user: Pick<User, 'name' | 'image'>
+interface Props extends AvatarProps {
+    user: Pick<User, "name" | "image">;
 }
 
-const UserAvatar = ({ user }: Props) => {
+const UserAvatar = ({ user,...props }: Props) => {
   return (
-    <Avatar>
+    <Avatar {...props}>
         {user.image ? (
             <div className='relative w-full h-full aspect-square'>
                 <Image 
