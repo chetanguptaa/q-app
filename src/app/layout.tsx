6733149d41f9cddb5f72aa4440353c265d62453e
chetan gupta <chetangupta123raj@gmail.com>
2023-10-01 11:14:43 +0530

    updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
-import { cn } from '@/lib/utils'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Providers from '@/components/Providers'
-import { Toaster } from '@/components/ui/toaster'
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'wacky-wisdom',
-  description: 'Wacky wisdom',
-}
+  title: "Quiz",
+  description: "Quiz-app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={
-        cn(inter.className, 'antialiased min-h-screen pt-16')}>
-          <Providers>
-            <Navbar />
-            {children}
-            <Toaster />
-          </Providers>
+      <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
+        <Providers>
+          <Navbar />
+          {children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
